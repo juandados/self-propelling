fc86d7a9a8bd7500b3ebcbd5f4da1fa62a1593c9
juan <juandados@gmail.com>
2019-03-23 23:29:38 -0700

    including moving polygons, 3 diferent forces leader individuals based on: 1) distance to polygon (f_polygon_1), 2) distance to a single point inside of the poligon (f_polygon_2), 3) distance to walls (f_polygon)

--- selfPropelling.m
+++ selfPropelling.m
@@ -5,27 +5,7 @@ end
 % Defining Default Parameters
 dt = 0.1;
 T = 100;
-
-if type == "pull_in_square"
-    % type specific variables
-    n = 9;
-    nw = 5;
-    % Reading InputStruct:
-    names = fieldnames(inputStruct);
-    for i=1:length(names)
-        eval([names{i} '=inputStruct.' names{i} ';' ]);
-    end
-    % Defining Initial Values
-    X = 0.001*rand(n,2)+2*ones(n,2);
-    V = 0.001*rand(n,2);
-    % Defining Boundary
-    x = linspace(0,1,nw)'; y = linspace(0,1,nw)';
-    neg_leaders = [zeros(nw,1),y;...
-        x(2:end-1),ones(nw-2,1);...
-        ones(nw,1),y(end:-1:1);...
-        x(end-1:-1:2),zeros(nw-2,1)];
-    f = @f_pull_in_square;
-end
+path_flag = false;
 
 if type == "polygon"
     n = 9;
@@ -40,7 +20,7 @@ if type == "polygon"
     X = 0.001*rand(n,2)+2*ones(n,2);
     V = 0.001*rand(n,2);
     % Defining Regular Polygon
-    f = @f_polygon;
+    f = @f_polygon_1;
 end
 
 if type == "star"
@@ -61,7 +41,7 @@ if type == "star"
     X = 0.001*rand(n,2)+2*ones(n,2);
     V = 0.001*rand(n,2);
     % Defining Regular Polygon
-    f = @f_polygon;
+    f = @f_polygon_1;
 end
 
 if type == "regular_polygon"
@@ -69,7 +49,6 @@ if type == "regular_polygon"
     % Reading InputStruct:
     names = fieldnames(inputStruct);
     n_s = 3;
-    % Defining Boundary
     for i=1:length(names)
         eval([names{i} '=inputStruct.' names{i} ';' ]);
     end
@@ -79,72 +58,43 @@ if type == "regular_polygon"
     % Defining Regular Polygon
     t = [-2*pi/n_s:2*pi/n_s:2*pi-4*pi/n_s]';
     neg_leaders = [sin(t),cos(t)];
-    f = @f_polygon;
-end
-
-
-if type == "square"
-    % type specific variables
-    n = 9;
-    nw = 5;
-    % Reading InputStruct:
-    names = fieldnames(inputStruct);
-    for i=1:length(names)
-        eval([names{i} '=inputStruct.' names{i} ';' ]);
-    end
-    % Defining Initial Values
-    X = 0.1*rand(n,2)+0.5*ones(n,2);
-    V = 0.001*rand(n,2);
-    % Defining Boundary
-    x = linspace(0,1,nw)'; y = linspace(0,1,nw)';
-    neg_leaders = [zeros(nw,1),y;...
-        x(2:end-1),ones(nw-2,1);...
-        ones(nw,1),y(end:-1:1);...
-        x(end-1:-1:2),zeros(nw-2,1)];
-    f = @f_square;
-end
-
-if type == "gravity"
-    n = 2;
-    % Reading InputStruct:
-    names = fieldnames(inputStruct);
-    neg_leaders = [0,0];
-    % Defining Boundary
-    for i=1:length(names)
-        eval([names{i} '=inputStruct.' names{i} ';' ]);
-    end
-    % Defining Initial Values
-    X = [0.5, 0.5; 0.4, 0.4];
-    X = [-0.5, 0.5];
-    %V = 0.1*[X(:,2), -X(:,1)]*2/sqrt(2);
-    V = 0.1*[X(:,2), -X(:,1)]*2/sqrt(2);
-    % Defining Regular Polygon
-    f = @f_gravity;
+    f = @f_polygon_1;
 end
 
 % Time evolution
 for t = 0:dt:T
-    [X,V] = runge_kutta_evolution(X, V, dt, f, neg_leaders);
+    path = path_flag*(t>10)*(t<30)*[0.1*(t-10),-0.1*(t-10)]; % choose n = 20
+    reposition = repmat(path,size(neg_leaders,1),1);
+    neg_leaders_t = neg_leaders + reposition;
+    [X,V] = runge_kutta_evolution(X, V, dt, f, neg_leaders_t);
+    if (apply(@(v)max(abs(v)), V(:))<1e-4) & false
+        disp('closed due to slow spead');
+        break
+    end
     figure(1);
     plot(X(:,1),X(:,2),'b.');
     hold on;
-    plot(neg_leaders([1:end,1],1), neg_leaders([1:end,1],2),'r-*');
-    %quiver(X(:,1),X(:,2),V(:,1),V(:,2));
-    axis([-2,2,-2,2]);
+    plot(neg_leaders_t([1:end,1],1), neg_leaders_t([1:end,1],2),'r:');
+    title(['t',num2str(t)])
+    %quiver(X(:,1),X(:,2),V(:,1),V(:,2),0);
+    axis([-5,5,-5,5]);
     axis square;
     hold off;
 end
 
 end
 
-function dY = f_gravity(Y, leaders)
-    p = 1;
-    rd = 1/1.5;
-    fI = @(r)0*0.00016./(r.^2);
-    %fh = @(r)0.01/(r.^2); % elliptic orbit;
-    fh = @(r)0.00707/(r.^2); % circular orbit;
-    vd = 0; a = 0;
-    d = size(Y,2); n = size(Y,1)/2; l=size(leaders,1);
+function dY = f_polygon_2(Y, vertices)
+    % fH depending on the distance from every individual to single point in
+    % the polygon interior
+    p = 8;
+    interior_point = [0,0];
+    d = size(Y,2); n = size(Y,1)/2; l=size(vertices,1);
+    area = polyarea(vertices(:,1),vertices(:,2));
+    rd = 2*sqrt(area/n);
+    fI = @(r)2*(r-rd).*(r-rd<0);
+    fh = @(r)(5*r).*(r>0);
+    vd = 0; a = 1.5;
     X = Y(1:n,:);
     V = Y(n+1:2*n,:);
     dX = V;
@@ -154,9 +104,13 @@ function dY = f_gravity(Y, leaders)
         r = apply(@(v)norm(v,p), R);
         DUsI = (fI(r)./r)*ones(1,d).*R;
         DUsI(i,:) = zeros(1,d);
-        H = ones(l,1)*X(i,:) - leaders;
-        h = apply(@(v)norm(v,p), H);
-        DUsH = (fh(h)./h)*ones(1,d).*H;
+        h = norm(X(i,:)-interior_point,2);
+        mask = inpolygon(X(i,1),X(i,2),vertices(:,1), vertices(:,2));
+        DUsH = (1-mask).*repmat(fh(h),1,d).*(X(i,:)-interior_point);
+        hold on;
+%        quiver(-DUsH(:,1)',-DUsH(:,2)',0);
+%        quiver(repmat(X(i,1),3,1), repmat(X(i,2),3,1), -H(:,1),-H(:,2),0);
+        hold off;
         dV2(i,:) = sum(DUsI,1) + sum(DUsH,1);
     end
     v = apply(@(v)norm(v,p), V);
@@ -164,6 +118,39 @@ function dY = f_gravity(Y, leaders)
     dV = dV1 - dV2;
     dY = [dX; dV];
 end
+
+function dY = f_polygon_1(Y, vertices)
+    % fH depending on the distance from every individual to the polygon
+    p = 8;
+    d = size(Y,2); n = size(Y,1)/2; l=size(vertices,1);
+    area = polyarea(vertices(:,1),vertices(:,2));
+    rd = 2*sqrt(area/n);
+    fI = @(r)2*(r-rd).*(r-rd<0);
+    fh = @(r)(10*r+2*l).*(r>0);
+    vd = 0; a = 1.5;
+    X = Y(1:n,:);
+    V = Y(n+1:2*n,:);
+    dX = V;
+    dV2 = zeros(size(V));
+    for i = 1:n
+        R = ones(n,1)*X(i,:) - X;
+        r = apply(@(v)norm(v,p), R);
+        DUsI = (fI(r)./r)*ones(1,d).*R;
+        DUsI(i,:) = zeros(1,d);
+        [h, x_poly,y_poly] = poly_dist(X(i,1),X(i,2),vertices(:,1),vertices(:,2));
+        DUsH = repmat(fh(h),1,d).*(X(i,:)-[x_poly, y_poly]);
+        hold on;
+%        quiver(-DUsH(:,1)',-DUsH(:,2)',0);
+%        quiver(repmat(X(i,1),3,1), repmat(X(i,2),3,1), -H(:,1),-H(:,2),0);
+        hold off;
+        dV2(i,:) = sum(DUsI,1) + sum(DUsH,1);
+    end
+    v = apply(@(v)norm(v,p), V);
+    dV1 = -a*((v-vd)./v)*ones(1,d).*V;
+    dV = dV1 - dV2;
+    dY = [dX; dV];
+end
+
 
 function dY = f_polygon(Y, vertices)
     p = 8;
@@ -193,60 +180,6 @@ function dY = f_polygon(Y, vertices)
         %quiver(repmat(X(i,1),3,1), repmat(X(i,2),3,1), DUsH(:,1),DUsH(:,2),0);
         %quiver(repmat(X(i,1),3,1), repmat(X(i,2),3,1), -H(:,1),-H(:,2),0);
         hold off;
-        dV2(i,:) = sum(DUsI,1) + sum(DUsH,1);
-    end
-    v = apply(@(v)norm(v,p), V);
-    dV1 = -a*((v-vd)./v)*ones(1,d).*V;
-    dV = dV1 - dV2;
-    dY = [dX; dV];
-end
-
-
-function dY = f_pull_in_square(Y, leaders)
-    p = 10;
-    rd = 1/1.5;
-    fI = @(r)2*(r-rd);
-    fh = @(r)2*(r-rd);
-    vd = 0; a = 1.5;
-    d = size(Y,2); n = size(Y,1)/2; l=size(leaders,1);
-    X = Y(1:n,:);
-    V = Y(n+1:2*n,:);
-    dX = V;
-    dV2 = zeros(size(V));
-    for i = 1:n
-        R = ones(n,1)*X(i,:) - X;
-        r = apply(@(v)norm(v,p), R);
-        DUsI = (fI(r)./r)*ones(1,d).*R;
-        DUsI(i,:) = zeros(1,d);
-        H = ones(l,1)*X(i,:) - leaders;
-        h = apply(@norm, H);
-        DUsH = (fh(h)./h)*ones(1,d).*H;
-        dV2(i,:) = sum(DUsI,1) + sum(DUsH,1);
-    end
-    v = apply(@(v)norm(v,p), V);
-    dV1 = -a*((v-vd)./v)*ones(1,d).*V;
-    dV = dV1 - dV2;
-    dY = [dX; dV];
-end
-
-function dY = f_square(Y, leaders)
-    d = size(Y,2); n = size(Y,1)/2; l=size(leaders,1);
-    p = 20; rd = 1/(sqrt(n)+1);
-    vd = 0; a = 1;
-    fI = @(r)(0.3*(r-rd).*(r<rd));
-    fh = @(r)(0.3*(r-rd).*(r<rd));
-    X = Y(1:n,:);
-    V = Y(n+1:2*n,:);
-    dX = V;
-    dV2 = zeros(size(V));
-    for i = 1:n
-        R = ones(n,1)*X(i,:) - X;
-        r = apply(@(v)norm(v,p), R);
-        DUsI = (fI(r)./r)*ones(1,d).*R;
-        DUsI(i,:) = zeros(1,d);
-        H = ones(l,1)*X(i,:) - leaders;
-        h = apply(@norm, H);
-        DUsH = (fh(h)./h)*ones(1,d).*H;
         dV2(i,:) = sum(DUsI,1) + sum(DUsH,1);
     end
     v = apply(@(v)norm(v,p), V);
